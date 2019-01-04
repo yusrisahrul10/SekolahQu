fbe06399c876889741c48f1866cc31464701538c
Unknown <helsanfirmansyah@gmail.com>
2019-01-04 19:36:39 +0700

    Update Logic Acara

--- app/src/main/java/com/dscunikom/android/sekolahqu/home/HomeFragment.kt
+++ app/src/main/java/com/dscunikom/android/sekolahqu/home/HomeFragment.kt
@@ -9,7 +9,7 @@ import android.view.ViewGroup
 import com.dscunikom.android.sekolahqu.R
 import com.dscunikom.android.sekolahqu.adapter.ViewPagerAdapter
 import com.dscunikom.android.sekolahqu.home.awards.AwardsFragment
-import com.dscunikom.android.sekolahqu.home.event.EventsFragment
+import com.dscunikom.android.sekolahqu.home.event.AcaraFragment
 import com.dscunikom.android.sekolahqu.home.news.BeritaFragment
 import com.dscunikom.android.sekolahqu.home.sekolah.sekolah.SekolahFragment
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -30,7 +30,7 @@ class HomeFragment : Fragment() {
                 mapOf(
                     getString(R.string.sekolah) to SekolahFragment(),
                     getString(R.string.berita) to BeritaFragment(),
-                    getString(R.string.acara) to EventsFragment(),
+                    getString(R.string.acara) to AcaraFragment(),
                     getString(R.string.prestasi) to AwardsFragment()
                 )
             )
--- app/src/main/java/com/dscunikom/android/sekolahqu/home/event/AcaraFragment.java
+++ app/src/main/java/com/dscunikom/android/sekolahqu/home/event/AcaraFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -20,25 +21,35 @@ import com.dscunikom.android.sekolahqu.model.acara.AcaraResponse;
 import com.dscunikom.android.sekolahqu.model.acara.AcaraModel;
 import com.dscunikom.android.sekolahqu.sharedpref.SessionManager;
 import com.dscunikom.android.sekolahqu.utils.RecyclerItemClickListener;
-import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class EventsFragment extends MvpFragment<AcaraPresenter> implements AcaraView {
+public class AcaraFragment extends MvpFragment<AcaraPresenter> implements AcaraView {
     RecyclerView recyclerView;
     private List<AcaraModel> mList;
     ImageView imgAcaraNews;
     TextView tvAcaraNews;
     SessionManager sessionManager;
+    SwipeRefreshLayout swipeRefresh;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.fragment_events, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_acara, container, false);
         presenter = createPresenter();
         recyclerView = rootView.findViewById(R.id.rv_events);
         tvAcaraNews = rootView.findViewById(R.id.text_fragment_acara);
         imgAcaraNews = rootView.findViewById(R.id.image_fragment_acara);
+        swipeRefresh = rootView.findViewById(R.id.swipe_acara);
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         recyclerView.addOnItemTouchListener(selectItemOnRecyclerView());
         recyclerView.setItemAnimator(new DefaultItemAnimator());
@@ -47,8 +58,12 @@ public class EventsFragment extends MvpFragment<AcaraPresenter> implements Acara
         String id_sekolah = sekolah.get(SessionManager.ID_SEKOLAH);
 //        FirebaseMessaging.getInstance().subscribeToTopic(id_sekolah);
         presenter.getDataAcara(id_sekolah);
-        return rootView;
-
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                presenter.getDataAcara(id_sekolah);
+            }
+        });
     }
 
     private RecyclerItemClickListener selectItemOnRecyclerView() {
@@ -64,6 +79,10 @@ public class EventsFragment extends MvpFragment<AcaraPresenter> implements Acara
                 presenter.getIdAcara(mList.get(position), activity);
             }
         });
+    }
+
+    private void clickFirstAcara(AcaraResponse model) {
+        presenter.getIdAcara(model.getmFirstData().get(0), activity);
     }
 
 
@@ -84,6 +103,7 @@ public class EventsFragment extends MvpFragment<AcaraPresenter> implements Acara
 
     @Override
     public void showListAcara(AcaraResponse model) {
+        swipeRefresh.setRefreshing(false);
         this.mList = model.getSpesifikSekolah();
         this.mList.remove(mList.get(0));
         recyclerView.setAdapter(new AcaraAdapter(mList,R.layout.item_content,this.getActivity()));
@@ -91,6 +111,7 @@ public class EventsFragment extends MvpFragment<AcaraPresenter> implements Acara
                 .load("http://sekolahqu.dscunikom.com/uploads/acara/"+model.getmFirstData().get(0).getImage())
                 .into(imgAcaraNews);
         tvAcaraNews.setText(model.getmFirstData().get(0).getNamaAcara());
+        imgAcaraNews.setOnClickListener(v -> clickFirstAcara(model));
     }
 
     @Override
